77e9eab945a44a5218bd2736ee22a7dcd592b5e0
Théau Méry <theaumery@gmail.com>
2021-05-12 17:01:25 +0200

    WIP: Stripe

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+require('dotenv').config();
 const stripe = require('stripe')(process.env.API_KEY_HIDDEN);
 const app = express()
 const port = 4242
 const bodyParser = require('body-parser')
 //Sert a utiliser les variables d'environnement
-require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
